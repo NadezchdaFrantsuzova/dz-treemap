bc2cca0db043ae8692052d40628ce8b5bc897542
Nadezcha Frantsuzova <nadia_franc@mail.ru>
2022-09-07 18:01:29 +0300

    Создан класс Main, класс Person, класс PersonComparator, файл .gitignore

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("Лидия", "Бех-Иванова", 45));
+        people.add(new Person("Эльдар", "Ахадов-ибн-Хабибов", 55));
+        people.add(new Person("Ольга", "Захарьева-Юрьева-Мамонтова-Иволгина", 70));
+        people.add(new Person("Алексей", "Петренко-Колотов-Румпель-Штицкин", 48));
+        people.add((new Person("Ольга", "Иванова", 60)));
+
+        Collections.sort(people, new PersonComparator(3));
+        System.out.println(people);
+    }
+}
