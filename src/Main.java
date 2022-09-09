4e56b721b11f6aceb50176b13e2917c669ff9056
Nadezcha Frantsuzova <nadia_franc@mail.ru>
2022-09-09 12:21:53 +0300

    В main добавлены people младше 18 лет, создана реализация удаления из списка людей младше 18 лет

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,11 @@ public class Main {
         people.add(new Person("Эльдар", "Ахадов-ибн-Хабибов", 55));
         people.add(new Person("Ольга", "Захарьева-Юрьева-Мамонтова-Иволгина", 70));
         people.add(new Person("Алексей", "Петренко-Колотов-Румпель-Штицкин", 48));
-        people.add((new Person("Ольга", "Иванова", 60)));
+        people.add(new Person("Ольга", "Иванова", 60));
+        people.add(new Person("Екатерина", "Сливко", 17));
+        people.add(new Person("Ольга", "Брюзгина-Нечепоренко", 10));
+
+        people.removeIf(person -> person.getAge() < 18);
 
         Collections.sort(people, new PersonComparator(3));
         System.out.println(people);
